d40a90b76c3ae11d2de47fdc4606ffce859b038a
asaqa988 <asaqa001@gmail.com>
2021-07-07 13:49:16 +0300

    3rd attempt

---
+++ app.js
@@ -0,0 +1,105 @@
+'use strict';
+let favclub = prompt('is real madrid my fav club?');
+if (favclub ==='yes'||favclub==='y')
+{
+
+ console.log('fav_club',favclub);
+    alert ('good job')
+} else if (favclub==='no' || favclub==='n')
+    {
+console.log ('fav_club', favclub);
+alert ('wrong answer');
+
+} else {
+console.log('please answr with yes or no')
+
+    }
+
+
+let favcolor = prompt('is blue my fav club?');
+if (favcolor ==='yes'||favcolor==='y')
+{
+
+ console.log('fav_club',favcolor);
+    alert ('good job')
+} else if (favcolor==='no' || favcolor==='n')
+    {
+console.log ('fav_club', favcolor);
+alert ('wrong answer');
+    
+    } else {
+console.log('please answr with yes or no')
+
+    }
+
+
+
+let major = prompt('is my major in security?');
+if (major ==='yes'||major==='y')
+{
+
+ console.log('my major',major);
+    alert ('good job')
+} else if (major==='no' || major==='n')
+    {
+console.log ('fav_club', major);
+alert ('wrong answer');
+
+    } else {
+console.log('please answr with yes or no')
+
+    }
+
+
+
+let nat = prompt('is my nationality is jordanian?');
+if (nat ==='yes'||nat==='y')
+{
+
+ console.log('my nationality',nat);
+    alert ('good job')
+} else if (nat==='no' || nat==='n')
+    {
+console.log ('my nationality', nat);
+alert ('wrong answer');
+
+} else {
+console.log('please answr with yes or no')
+
+    
+
+}
+
+let car = prompt('is my car a sonata?');
+if (car ==='yes'||car==='y')
+{
+
+ console.log('my car',car);
+    alert ('good job')
+} else if (car==='no' || car==='n')
+    {
+console.log ('my car', car);
+alert ('wrong answer');
+
+} else {
+console.log('please answr with yes or no')
+
+    
+
+}
+
+let food=prompt('is mansaf my fav food ?')
+food=food. toLowerCase();
+switch (food){
+case 'yes':
+case 'y':
+alert ('no you are wrong');
+console.log("my best dish",food);
+break;
+case 'no':
+case 'n':
+    alert('good job')
+    break;
+    default 
+    alert ('please selecet yes or no ');
+break;
